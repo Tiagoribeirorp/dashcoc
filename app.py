9d34fee860443e32fe6e8463ce82bf3e65c3fec6
Tiago Ribeiro <tiagoribeirodev@gmail.com>
2026-02-11 01:17:40 -0300

    test

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ pd.set_option('display.max_colwidth', 100)
 
 # Configurar página
 st.set_page_config(
-    page_title="Dashboard de Campanhas - SICOOB COCRED",
+    page_title="Dashboard de Campanhas - SICOOB COCRED1",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded"
